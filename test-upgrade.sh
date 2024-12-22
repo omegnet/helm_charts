702671fb02050e620ab6df371b7dacac1084b69f
florinen <45836669+florinen@users.noreply.github.com>
2024-12-21 19:36:20 -0500

    Workflow4 (#90)
    * id
    
    * id
    
    * added appversion
    
    * added repos
    
    * added repos
    
    * added app versioning
    
    * changed name
    
    ---------
    
    Co-authored-by: florin <nenfl@yahoo.com>

--- test-upgrade.sh
+++ test-upgrade.sh
@@ -13,20 +13,34 @@ if [ ! -f "$CONFIG_FILE" ]; then
 fi
 
 # Read the chart_versions.yaml file using yq
-CHARTS=$(yq -r '.charts[] | .chart + " " + .version' "$CONFIG_FILE")
+CHARTS=$(yq -r '.charts[] | .chart + " " + .version + " " + .appVersion' "$CONFIG_FILE")
 PACKAGES_DIR="./.packages"
 
 # Loop through each chart and process it
 while IFS= read -r CHART_VERSION; do
     CHART=$(echo "$CHART_VERSION" | awk '{print $1}')  # Chart name
     VERSION=$(echo "$CHART_VERSION" | awk '{print $2}')  # Desired version
+    APP_VERSION=$(echo "$CHART_VERSION" | awk '{print $3}')  # Desired app version
+    CHART_NAME=$(echo "$CHART_VERSION" | awk '{print $1}' |awk -F '/' '{print $2}')
+    CURRENT_APP_VERSION=$(grep "^appVersion:" "./charts/$CHART_NAME/Chart.yaml" | awk '{print $2}')
 
     echo "Processing chart: $CHART"
     echo "Upgrading to version: $VERSION"
     if helm show chart "$CHART" --version "$VERSION" >/dev/null 2>&1; then
         echo "Version $VERSION for $CHART found. Proceeding..."
-         helm pull "$CHART" --version "$VERSION" -d "$PACKAGES_DIR"
-         helm repo index "$PACKAGES_DIR" --url https://omegnet.github.io/helm_charts/ 
+        if [ -n "$VERSION" ]; then
+            echo "Upgrading Chart Version to: $VERSION"
+            sed -i "s/^version:.*/version: $VERSION/" ./charts/$CHART_NAME/Chart.yaml
+        fi
+
+        if [ -n "$APP_VERSION" ]; then
+            if [ "$APP_VERSION" != "$CURRENT_VERSION" ]; then
+                echo "Upgrading appVersion to: $APP_VERSION"
+                sed -i "s/^appVersion:.*/appVersion: $APP_VERSION/" ./charts/$CHART_NAME/Chart.yaml
+            fi
+        fi
+        #  helm pull "$CHART" --version "$VERSION" -d "$PACKAGES_DIR"
+        #  helm repo index "$PACKAGES_DIR" --url https://omegnet.github.io/helm_charts/ 
     else
         echo "Version $VERSION for $CHART not found. Skipping..."
         continue
